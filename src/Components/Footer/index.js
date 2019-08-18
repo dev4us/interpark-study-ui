b7c47f91daa7cd645e12cf103863da10b95cfec2
dev4us <bronxofsoul@gmail.com>
2019-08-18 19:05:05 +0900

    [FE] sticky to fixed

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,11 +1,11 @@
 import React from "react";
-import styled, {css} from "styled-components";
+import styled, { css } from "styled-components";
 import { Link, withRouter } from "react-router-dom";
 
 import { MdPeople, MdMap, MdTextsms } from "react-icons/md";
 import { FaPlusCircle } from "react-icons/fa";
 
-const Footer = ({location}) => {
+const Footer = ({ location }) => {
   const locationPath = location.pathname;
 
   return (
@@ -13,51 +13,73 @@ const Footer = ({location}) => {
       <Container>
         <StickyBar>
           <FooterFrame>
-            <FooterIcon to="/main" active={locationPath === '/main' ? 'active' : ''}><MdTextsms /></FooterIcon>
-            <FooterIcon to="/posts" active={locationPath === '/posts' ? 'active' : ''}><MdMap/></FooterIcon>
-            <FooterIcon to="/write" active={locationPath === '/write' ? 'active' : ''}><FaPlusCircle/></FooterIcon>
-            <FooterIcon to="/mypage" active={locationPath === '/mypage' ? 'active' : ''}><MdPeople/></FooterIcon>
+            <FooterIcon
+              to="/main"
+              active={locationPath === "/main" ? "active" : ""}
+            >
+              <MdTextsms />
+            </FooterIcon>
+            <FooterIcon
+              to="/posts"
+              active={locationPath === "/posts" ? "active" : ""}
+            >
+              <MdMap />
+            </FooterIcon>
+            <FooterIcon
+              to="/write"
+              active={locationPath === "/write" ? "active" : ""}
+            >
+              <FaPlusCircle />
+            </FooterIcon>
+            <FooterIcon
+              to="/mypage"
+              active={locationPath === "/mypage" ? "active" : ""}
+            >
+              <MdPeople />
+            </FooterIcon>
           </FooterFrame>
-         </StickyBar>
-      </Container> 
+        </StickyBar>
+      </Container>
     </>
   );
-}
+};
 export default withRouter(Footer);
 
 const Container = styled.div`
-  position:sticky;
-  width:100%;
-  bottom:0;
-  z-index:9999;
+  position: fixed;
+  width: 100%;
+  bottom: 0;
+  z-index: 9999;
 `;
 
 const StickyBar = styled.div`
-  width:100%;
-  height:3rem;
-  background:white;
+  width: 100%;
+  height: 3rem;
+  background: white;
 `;
 
 const FooterFrame = styled.div`
-  display:flex;
-  justify-content:center;
-  align-items:center;
-  width:100%;
-  height:100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
 `;
 
 const FooterIcon = styled(Link)`
-  display:flex;
-  justify-content:center;
-  align-items:center;
-  height:100%;
-  flex:0.25;
-  font-size:1.2rem;
-  color:#b3b2b2;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  flex: 0.25;
+  font-size: 1.2rem;
+  color: #b3b2b2;
 
-  ${props => props.active !== '' && css`
-    color:#424242;
-    border-bottom:3px solid #0978fe;
-    padding-top:3.2px;
-  `}
+  ${props =>
+    props.active !== "" &&
+    css`
+      color: #424242;
+      border-bottom: 3px solid #0978fe;
+      padding-top: 3.2px;
+    `}
 `;
